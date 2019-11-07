64f4b60d5e16adf78954ab811d537c5fab32eab5
Muhammad Farabi <mfaarabi@gmail.com>
2019-11-07 22:48:28 +0700

    Refactor test file

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-  expected := "Hello, Farabi"
-  actual := Hello("Farabi")
+	assertCorrectMessage := func(t *testing.T, expected, actual string) {
+		t.Helper()
+		if expected != actual {
+			t.Errorf("Expected %q, actual %q.", expected, actual)
+		}
+	}
 
-  if expected != actual {
-    t.Errorf("Expected %q, actual %q.", expected, actual)
-  }
+	t.Run("Hello with params", func(t *testing.T) {
+		expected := "Hello, Farabi"
+		actual := Hello("Farabi")
+		assertCorrectMessage(t, expected, actual)
+	})
+
+	t.Run("Hello with empty string", func(t *testing.T) {
+		expected := "Hello, "
+		actual := Hello("")
+		assertCorrectMessage(t, expected, actual)
+	})
 }
