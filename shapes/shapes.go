7f100d43c63d2c157eee0ffe891fc89bb9120153
Muhammad Farabi <mfaarabi@gmail.com>
2019-12-24 19:07:11 +0700

    [Structs Methods Interfaces] Refactor shapes to accept Rectangle struct as parameter

--- shapes/shapes.go
+++ shapes/shapes.go
@@ -1,9 +1,18 @@
 package shapes
 
-func Perimeter(width, height float64) float64 {
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	width := rectangle.Width
+	height := rectangle.Height
 	return 2 * (width + height)
 }
 
-func Area(width, height float64) float64 {
+func Area(rectangle Rectangle) float64 {
+	width := rectangle.Width
+	height := rectangle.Height
 	return width * height
 }
--- shapes/shapes_test.go
+++ shapes/shapes_test.go
@@ -8,14 +8,14 @@ import (
 
 func TestPerimeter(t *testing.T) {
 	expected := 40.0
-	actual := Perimeter(10.0, 10.0)
+	actual := Perimeter(Rectangle{10.0, 10.0})
 
 	assert.Equal(t, expected, actual)
 }
 
 func TestArea(t *testing.T) {
 	expected := 72.0
-	actual := Area(12.0, 6.0)
+	actual := Area(Rectangle{12.0, 6.0})
 
 	assert.Equal(t, expected, actual)
 }
