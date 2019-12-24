fa666d61231d3aad548060e4ae43845331f6fc50
Muhammad Farabi <mfaarabi@gmail.com>
2019-12-25 06:56:56 +0700

    [Pointers & Errors] Add test for Withdraw & add assertBalance helper

--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -3,13 +3,30 @@ package wallet
 import "testing"
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(Bitcoin(10))
-	expected := Bitcoin(10)
-
-	actual := wallet.Balance()
-
-	if expected != actual {
-		t.Errorf("Was expecting %s but got %s instead", expected, actual)
+	assertBalance := func(t *testing.T, expected, actual Bitcoin) {
+		t.Helper()
+		if expected != actual {
+			t.Errorf("Was expecting %s but got %s instead", expected, actual)
+		}
 	}
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		expected := Bitcoin(10)
+
+		actual := wallet.Balance()
+
+		assertBalance(t, expected, actual)
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(10))
+		expected := Bitcoin(10)
+
+		actual := wallet.Balance()
+
+		assertBalance(t, expected, actual)
+	})
 }
