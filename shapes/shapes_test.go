82349c749fc697d2764c165c8fad33e6ef55a049
Muhammad Farabi <mfaarabi@gmail.com>
2019-12-24 21:40:56 +0700

    [Structs Methods Interfaces] Refactor table test with t.Run identified by table row's name

--- shapes/shapes_test.go
+++ shapes/shapes_test.go
@@ -15,16 +15,19 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 	areaTests := []struct {
-		shape    Shape
-		expected float64
+		name    string
+		shape   Shape
+		hasArea float64
 	}{
-		{Rectangle{12.0, 6.0}, 72.0},
-		{Circle{10}, 314.1592653589793},
-		{Triangle{12.0, 6.0}, 36.0},
+		{name: "Rectangle", shape: Rectangle{12.0, 6.0}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{12.0, 6.0}, hasArea: 36.0},
 	}
 
 	for _, tt := range areaTests {
-		actual := tt.shape.Area()
-		assert.Equal(t, tt.expected, actual)
+		t.Run(tt.name, func(t *testing.T) {
+			actual := tt.shape.Area()
+			assert.Equal(t, tt.hasArea, actual)
+		})
 	}
 }
