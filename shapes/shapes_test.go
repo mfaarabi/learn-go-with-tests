873ce12b039dfc5189e793ac9a8863ab08adaeb0
Muhammad Farabi <mfaarabi@gmail.com>
2019-12-24 21:28:49 +0700

    [Structs Methods Interfaces] Refactor TestArea in a table driven test manner

--- shapes/shapes_test.go
+++ shapes/shapes_test.go
@@ -14,20 +14,16 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	checkArea := func(t *testing.T, shape Shape, expected float64) {
-		t.Helper()
-		assert.Equal(t, shape.Area, expected)
+	areaTests := []struct {
+		shape    Shape
+		expected float64
+	}{
+		{Rectangle{12.0, 6.0}, 72.0},
+		{Circle{10}, 314.1592653589793},
 	}
 
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 6.0}
-		expected := 72.0
-		checkArea(t, rectangle, expected)
-	})
-
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		expected := 314.1592653589793
-		checkArea(t, circle, expected)
-	})
+	for _, tt := range areaTests {
+		actual := tt.shape.Area()
+		assert.Equal(t, tt.expected, actual)
+	}
 }
