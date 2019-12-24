c95b9c5485b0199f3615c19489fdcee1f439641e
Muhammad Farabi <mfaarabi@gmail.com>
2019-12-24 19:26:04 +0700

    [Structs Methods Interfaces] Refactor shapes to change from function to method
    - Accept Circle and Rectangle struct as receiver
    - Add spec for Area of Circle

--- shapes/shapes.go
+++ shapes/shapes.go
@@ -1,5 +1,7 @@
 package shapes
 
+import "math"
+
 type Rectangle struct {
 	Width  float64
 	Height float64
@@ -11,8 +13,17 @@ func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (width + height)
 }
 
-func Area(rectangle Rectangle) float64 {
-	width := rectangle.Width
-	height := rectangle.Height
+func (r Rectangle) Area() float64 {
+	width := r.Width
+	height := r.Height
 	return width * height
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	radius := c.Radius
+	return math.Pi * math.Pow(radius, 2)
 }
--- shapes/shapes_test.go
+++ shapes/shapes_test.go
@@ -14,8 +14,19 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	expected := 72.0
-	actual := Area(Rectangle{12.0, 6.0})
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12.0, 6.0}
+		expected := 72.0
+		actual := rectangle.Area()
 
-	assert.Equal(t, expected, actual)
+		assert.Equal(t, expected, actual)
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		expected := 314.1592653589793
+		actual := circle.Area()
+
+		assert.Equal(t, expected, actual)
+	})
 }
