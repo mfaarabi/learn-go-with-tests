2e7b01a750699d63a4d9e5957d5c3d333b249b9f
Muhammad Farabi <mfaarabi@gmail.com>
2020-01-04 14:47:33 +0700

    [Dependency Injection] Demonstrate DI by using Writer implementation of net/http

--- main/greet.go
+++ main/greet.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"net/http"
 	"os"
 
 	"github.com/mfaarabi/learn-go-with-tests/greet"
 )
 
+const theString = "Wendy\n"
+const port = ":5000"
+
+func HTTPGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	greet.Greet(w, theString)
+}
+
 func main() {
-	greet.Greet(os.Stdout, "Wendy\n")
+	greet.Greet(os.Stdout, theString)
+	http.ListenAndServe(port, http.HandlerFunc(HTTPGreeterHandler))
 }
